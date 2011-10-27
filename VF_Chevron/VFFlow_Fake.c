875d130930c5d786f06d24e35fd2cf200676b99c
Chukwudozie Chukwudi <chuckdii2002@yahoo.com>
2011-10-27 11:55:16 -0500

    Added a new fake flow Pressure profile is step function such that it is equal to Pinit outside an elliptic region and higher inside the ellipse

--- VF_Chevron/VFFlow_Fake.c
+++ VF_Chevron/VFFlow_Fake.c
@@ -13,66 +13,63 @@
 /* 
    Fake flow solver for VF_Chevron.c test
 */
+
 extern PetscErrorCode VFFlow_Fake(VFCtx *ctx, VFFields *fields)
 {
-  PetscErrorCode ierr;
-  PetscReal      time;
-  PetscReal      pres,temp;
-  PetscInt       xs,xm,nx;
-  PetscInt       ys,ym,ny;
-  PetscInt       zs,zm,nz;
-  PetscInt       i,j,k;
-  Vec            theta_localVec;
-  Vec            pressure_localVec;
-  PetscReal      ***theta_array;
-  PetscReal      ***pressure_array;
-  PetscReal      ****coords_array;
-  PetscReal      x,y,z,r;
-  PetscReal      Tinit,Pinit,P1,P2,T1,T2;
+	PetscErrorCode ierr;
+	PetscReal			time;
+	PetscReal			min_radius, max_radius, alpha = 0.5;
+	PetscReal			dist;
+	PetscReal			incremnt = 2.;
+	PetscInt			xs,xm,nx;
+	PetscInt			ys,ym,ny;
+	PetscInt			zs,zm,nz;
+	PetscInt			ei,ej,ek;
+	PetscReal			***pressure_array;
+	PetscReal			****coords_array;
+	PetscReal			Pinit;
+	
+	
 
-  PetscFunctionBegin;
-  
-  if (ctx->verbose >0) {
-    ierr = PetscPrintf(PETSC_COMM_WORLD, "Entering flow solver %s implemented in %s\n",__FUNCT__,__FILE__);CHKERRQ(ierr);
-  }
+	
+	/*Fake flow where the pressure in the reservoir has an elliptic profile (profile is independent of z axis) 
+	 so that the principal radii (min_radius and max_radius) of the profile are a functions of time*/
+	
+	
+	PetscFunctionBegin;
+	if (ctx->verbose >0) {
+		ierr = PetscPrintf(PETSC_COMM_WORLD, "Entering flow solver %s implemented in %s\n",__FUNCT__,__FILE__);CHKERRQ(ierr);
+	}
+	
+	//printf("/n................This is fake flow................../n");
+	
+	ierr = DAGetInfo(ctx->daVect,PETSC_NULL,&nx,&ny,&nz,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+	ierr = DAGetCorners(ctx->daVect,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
+	
+	
+	ierr = DAVecGetArrayDOF(ctx->daVect,ctx->coordinates,&coords_array);CHKERRQ(ierr);
+	ierr = DAVecGetArray(ctx->daScal,fields->pressure,&pressure_array);CHKERRQ(ierr);    
+	
+	
+	time = ctx->timevalue;
+	Pinit = ctx->resprop.Pinit;
+	min_radius = 0.5*(1.0-exp(-time*alpha));
+	max_radius = 0.2*(1.0-exp(-time*alpha));
+	
+	for (ek = zs; ek < zs + zm; ek++) {
+		for (ej = ys; ej < ys+ym; ej++) {
+			for (ei = xs; ei < xs+xm; ei++) {
+				dist = pow( ((coords_array[ek][ej][ei][0]-0.5)/min_radius), 2) + pow(((coords_array[ek][ej][ei][1]-0.5)/max_radius), 2);
+				if( dist <= 1 )
+					pressure_array[ek][ej][ei] = incremnt * Pinit;
+				else
+					pressure_array[ek][ej][ei] = Pinit;
+			}
+		}
+	}
+	
+	ierr = DAVecRestoreArrayDOF(ctx->daVect,ctx->coordinates,&coords_array);CHKERRQ(ierr);
+	ierr = DAVecRestoreArray(ctx->daScal,fields->pressure,&pressure_array);CHKERRQ(ierr);
 
-  ierr = DAGetInfo(ctx->daVect,PETSC_NULL,&nx,&ny,&nz,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
-  ierr = DAGetCorners(ctx->daVect,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
-
-  /*
-     Get coordinate, temperature array, and pressure array
-  */
-  ierr = DAVecGetArrayDOF(ctx->daVect,ctx->coordinates,&coords_array);CHKERRQ(ierr);
-  ierr = DAVecGetArray(ctx->daScal,fields->theta,&theta_array);CHKERRQ(ierr);
-  ierr = DAVecGetArray(ctx->daScal,fields->pressure,&pressure_array);CHKERRQ(ierr);
-
-  time = ctx->timevalue;
-  Tinit = ctx->resprop.Tinit;
-  Pinit = ctx->resprop.Pinit;
-  P1 = 1.;
-  P2 = 1.e6;
-  T1 = 2.;
-  T2 = 1.e6;
-
-  for (k = zs; k < zs + zm; k++) {
-    for (j = ys; j < ys + ym; j++) {
-      for (i = xs; i < xs + xm; i++) {
-        x = coords_array[k][j][i][0]+0.5;
-        y = coords_array[k][j][i][1]+0.5;
-        z = coords_array[k][j][i][2]+0.5;
-        r = sqrt(x*x+y*y);
-        theta_array[k][j][i] = Tinit - T1*log(T2*time/(r*r));
-        pressure_array[k][j][i] = Pinit + P1*log(P2*time/(r*r));
-      }
-    }
-  } 
-  
-  /*
-     Cleanup
-  */
-  ierr = DAVecRestoreArrayDOF(ctx->daVect,ctx->coordinates,&coords_array);CHKERRQ(ierr);
-  ierr = DAVecRestoreArray(ctx->daScal,fields->theta,&theta_array);CHKERRQ(ierr);
-  ierr = DAVecRestoreArray(ctx->daScal,fields->pressure,&pressure_array);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
+	PetscFunctionReturn(0);	
 }
-
