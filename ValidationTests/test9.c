5a91848123690080bc9c8a859be7d4b1a8dd3af0
Blaise Bourdin <bourdin@lsu.edu>
2012-03-28 21:09:35 -0500

    used uncrustify to clean up indentation in examples

--- ValidationTests/test9.c
+++ ValidationTests/test9.c
@@ -11,107 +11,107 @@
 #include "VFU.h"
 #include "VFFlow.h"
 
-VFCtx               ctx;
-VFFields            fields;
+VFCtx    ctx;
+VFFields fields;
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
 int main(int argc,char **argv)
 {
-  VFCtx               ctx;
-  VFFields            fields;
-  PetscErrorCode      ierr;
-  
-  PetscInt            i,j,k,c,nx,ny,nz,xs,xm,ys,ym,zs,zm;
-  PetscReal       ****coords_array;
-  PetscReal           ElasticEnergy = 0;
-  PetscReal           InsituWork = 0;
-  PetscReal           SurfaceEnergy = 0;
-  char                filename[FILENAME_MAX];
-  PetscReal           p = 1e-3;
-  PetscReal           x[3],xc[3];
-  PetscReal           dist,d;
-  VFPennyCrack       *crack;
-  PetscInt            nc=0;
-  char                prefix[PETSC_MAX_PATH_LEN+1];
-  Vec                 V;
-  
+  VFCtx          ctx;
+  VFFields       fields;
+  PetscErrorCode ierr;
+
+  PetscInt     i,j,k,c,nx,ny,nz,xs,xm,ys,ym,zs,zm;
+  PetscReal    ****coords_array;
+  PetscReal    ElasticEnergy = 0;
+  PetscReal    InsituWork    = 0;
+  PetscReal    SurfaceEnergy = 0;
+  char         filename[FILENAME_MAX];
+  PetscReal    p = 1e-3;
+  PetscReal    x[3],xc[3];
+  PetscReal    dist,d;
+  VFPennyCrack *crack;
+  PetscInt     nc = 0;
+  char         prefix[PETSC_MAX_PATH_LEN+1];
+  Vec          V;
+
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,banner);CHKERRQ(ierr);
   ierr = VFInitialize(&ctx,&fields);CHKERRQ(ierr);
-  
+
   ierr = PetscOptionsGetInt(PETSC_NULL,"-nc",&nc,PETSC_NULL);CHKERRQ(ierr);
   ierr = DMDAGetInfo(ctx.daScal,PETSC_NULL,&nx,&ny,&nz,PETSC_NULL,PETSC_NULL,PETSC_NULL,
-                    PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
-	ierr = DMDAGetCorners(ctx.daScal,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
+                     PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+  ierr = DMDAGetCorners(ctx.daScal,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
 
   ierr = PetscMalloc(nc*sizeof(VFPennyCrack),&crack);CHKERRQ(ierr);
   for (i = 0; i < nc; i++) {
     ierr = PetscSNPrintf(prefix,PETSC_MAX_PATH_LEN,"c%d_",i);CHKERRQ(ierr);
     ierr = VFPennyCrackCreate(&crack[i]);CHKERRQ(ierr);
-    ierr = VFPennyCrackGet(prefix, &crack[i]);CHKERRQ(ierr);
+    ierr = VFPennyCrackGet(prefix,&crack[i]);CHKERRQ(ierr);
     ierr = VFPennyCrackView(&crack[i],PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
   }
-	
-	ierr = DMDAVecGetArrayDOF(ctx.daVect,ctx.coordinates,&coords_array);CHKERRQ(ierr);
-	
+
+  ierr = DMDAVecGetArrayDOF(ctx.daVect,ctx.coordinates,&coords_array);CHKERRQ(ierr);
+
   ierr = DMCreateGlobalVector(ctx.daScal,&V);CHKERRQ(ierr);
   ierr = VecSet(V,1.0);CHKERRQ(ierr);
 
-	ierr = VecSet(fields.V,1.0);CHKERRQ(ierr);
-	
+  ierr = VecSet(fields.V,1.0);CHKERRQ(ierr);
+
   /*
     Reset all BC for U and V
   */
   for (i = 0; i < 6; i++) {
-    ctx.bcV[0].face[i]=NONE;
+    ctx.bcV[0].face[i] = NONE;
     for (j = 0; j < 3; j++) {
       ctx.bcU[j].face[i] = NONE;
     }
   }
   for (i = 0; i < 12; i++) {
-    ctx.bcV[0].edge[i]=NONE;
+    ctx.bcV[0].edge[i] = NONE;
     for (j = 0; j < 3; j++) {
       ctx.bcU[j].edge[i] = NONE;
     }
   }
   for (i = 0; i < 8; i++) {
-    ctx.bcV[0].vertex[i]=NONE;
+    ctx.bcV[0].vertex[i] = NONE;
     for (j = 0; j < 3; j++) {
       ctx.bcU[j].vertex[i] = NONE;
     }
   }
-  
+
   ctx.bcU[0].face[X0] = ZERO;
   ctx.bcU[0].face[X1] = ZERO;
   ctx.bcU[1].face[Y0] = ZERO;
   ctx.bcU[1].face[Y1] = ZERO;
   ctx.bcU[2].face[Z0] = ZERO;
   ctx.bcU[2].face[Z1] = ZERO;
-  
+
   for (c = 0; c < nc; c++) {
     ierr = VFPennyCrackBuildVAT2(V,&crack[c],&ctx);CHKERRQ(ierr);
     ierr = VecPointwiseMin(fields.V,V,fields.V);CHKERRQ(ierr);
   }
   ierr = VecDestroy(&V);CHKERRQ(ierr);
-	ierr = VecCopy(fields.V,fields.VIrrev);CHKERRQ(ierr);
-  
+  ierr = VecCopy(fields.V,fields.VIrrev);CHKERRQ(ierr);
+
   ierr = VecSet(fields.theta,0.0);CHKERRQ(ierr);
   ierr = VecSet(fields.thetaRef,0.0);CHKERRQ(ierr);
   ierr = VecSet(fields.pressure,p);CHKERRQ(ierr);
   ierr = VecSet(fields.pressureRef,0.0);CHKERRQ(ierr);
 
-  //ierr = BCUUpdate(&ctx.bcU[0],ctx.preset);CHKERRQ(ierr);
+  /* ierr = BCUUpdate(&ctx.bcU[0],ctx.preset);CHKERRQ(ierr); */
   ctx.hasCrackPressure = PETSC_TRUE;
-  ierr = VF_StepU(&fields,&ctx);
-  ctx.ElasticEnergy=0;
-  ctx.InsituWork=0;
-  ctx.PressureWork = 0.;
-  ierr = VF_UEnergy3D(&ctx.ElasticEnergy,&ctx.InsituWork,&ctx.PressureWork,&fields,&ctx);CHKERRQ(ierr);
-  ctx.TotalEnergy = ctx.ElasticEnergy - ctx.InsituWork - ctx.PressureWork;
-  
-  
-  
+  ierr                 = VF_StepU(&fields,&ctx);
+  ctx.ElasticEnergy    = 0;
+  ctx.InsituWork       = 0;
+  ctx.PressureWork     = 0.;
+  ierr                 = VF_UEnergy3D(&ctx.ElasticEnergy,&ctx.InsituWork,&ctx.PressureWork,&fields,&ctx);CHKERRQ(ierr);
+  ctx.TotalEnergy      = ctx.ElasticEnergy-ctx.InsituWork-ctx.PressureWork;
+
+
+
   ierr = PetscPrintf(PETSC_COMM_WORLD,"Elastic Energy:            %e\n",ctx.ElasticEnergy);CHKERRQ(ierr);
   if (ctx.hasCrackPressure) {
     ierr = PetscPrintf(PETSC_COMM_WORLD,"Work of pressure forces:   %e\n",ctx.PressureWork);CHKERRQ(ierr);
@@ -123,24 +123,25 @@ int main(int argc,char **argv)
 
 
 
-    /*
-      Save fields and write statistics about current run
-    */    
-    switch (ctx.fileformat) {
-      case FILEFORMAT_HDF5:       
-        ierr = FieldsH5Write(&ctx,&fields);
-        ctx.timestep++;
-        ctx.timevalue += 1.;
-        ierr = FieldsH5Write(&ctx,&fields);
-      break;
-      case FILEFORMAT_BIN:
-        ierr = FieldsBinaryWrite(&ctx,&fields);
-      break; 
-    } 
+  /*
+    Save fields and write statistics about current run
+  */
+  switch (ctx.fileformat) {
+  case FILEFORMAT_HDF5:
+    ierr = FieldsH5Write(&ctx,&fields);
+    ctx.timestep++;
+    ctx.timevalue += 1.;
+    ierr           = FieldsH5Write(&ctx,&fields);
+    break;
+
+  case FILEFORMAT_BIN:
+    ierr = FieldsBinaryWrite(&ctx,&fields);
+    break;
+  }
 
   ierr = PetscFree(crack);CHKERRQ(ierr);
   ierr = VFFinalize(&ctx,&fields);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return(0);
 }
-  
+
