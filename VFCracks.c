55dc796ebace07226539e60b2458d88dd048e596
Blaise Bourdin <bourdin@lsu.edu>
2012-05-01 21:09:51 -0500

    commented out unfinished work

--- VFCracks.c
+++ VFCracks.c
@@ -176,126 +176,126 @@ extern PetscErrorCode VFPennyCrackBuildVAT2(Vec V,VFPennyCrack *crack,VFCtx *ctx
   PetscFunctionReturn(0);
 }
 
-#undef __FUNCT__
-#define __FUNCT__ "VFRectangularCrackGet"
-/*
-  
-
-  VFRectangularCrackGet (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
-*/
-extern PetscErrorCode VFRectangularCrackGet(const char prefix[],VFRectangularCrack *RectangularCrack)
-{
-  PetscErrorCode ierr;
-  PetscInt       nval=3;
-  PetscFunctionBegin;
-  ierr = PetscOptionsBegin(PETSC_COMM_WORLD,prefix,"\n\nVF: Rectangular-shaped crack description:","");CHKERRQ(ierr);
-  {
-    ierr = PetscOptionsString("-name","\n\tRectangular-shaped crack name","",RectangularCrack->name,RectangularCrack->name,sizeof(RectangularCrack->name),PETSC_NULL);CHKERRQ(ierr);
-    nval = 9;    
-    ierr = PetscOptionsRealArray("-corners","\n\tRectangular-shaped crack corners coordinates (x0,y0,z0, x1,y1,z1, x2,y2,z2)  (comma separated).","",RectangularCrack->center,&nval,PETSC_NULL);CHKERRQ(ierr);
-  }
-  ierr = PetscOptionsEnd();CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "VFRectangularCrackCreate"
-/*
-  VFRectangularCrackCreate: Allocates a RectangularCrack data structure
-  
-  (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
-*/
-extern PetscErrorCode VFRectangularCrackCreate(VFRectangularCrack *RectangularCrack)
-{
-  PetscErrorCode ierr;
-  int            i;
-  
-  PetscFunctionBegin;
-  ierr = PetscStrcpy(RectangularCrack->name,"RectangularCrack");CHKERRQ(ierr);
-  for (i=0; i<9; i++) RectangularCrack->corners[i] = 0.;
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "VFRectangularCrackView"
-/*
-  VFRectangularCrackView 
-  
-  (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
-*/
-extern PetscErrorCode VFRectangularCrackView(VFRectangularCrack *RectangularCrack,PetscViewer viewer)
-{
-  PetscErrorCode ierr;
-  
-  PetscFunctionBegin;
-  ierr = PetscViewerASCIIPrintf(viewer,"RectangularCrack object \"%s\":\n",RectangularCrack->name);CHKERRQ(ierr);
-  ierr = PetscViewerASCIIPrintf(viewer,"corners:     \t%e \t%e \t%e \t%e\n\t\t%e \t%e \t%e \t%e\n\t\t%e \t%e \t%e \t%e\n",
-                               RectangularCrack->corners[0],RectangularCrack->corners[1],RectangularCrack->corners[2],
-                               RectangularCrack->corners[3],RectangularCrack->corners[4],RectangularCrack->corners[5],
-                               RectangularCrack->corners[6],RectangularCrack->corners[7],RectangularCrack->corners[8]);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "VFRectangularCrackSetName"
-/*
-  VFRectangularCrackSetName 
-  
-  (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
-*/
-extern PetscErrorCode VFRectangularCrackSetName(VFRectangularCrack *RectangularCrack,const char name[])
-{
-  PetscErrorCode ierr;
-  
-  PetscFunctionBegin;
-  ierr = PetscStrcpy(RectangularCrack->name,name);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "VFDistanceToRectangularCrack"
-/*
-  VFDistanceToRectangularCrack: Computes the distance between a point with coordinates x and a RectangularCrack described by RectangularCrack
-  
-  
-  (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
-*/
-extern PetscErrorCode VFDistanceToRectangularCrack(PetscReal *d,PetscReal *x,VFRectangularCrack *Crack)
-{
-  PetscErrorCode      ierr;
-  PetscReal           n[3],tau[3],l,xdotn;
-
-  PetscFunctionBegin;
-  /*
-    n: normal vector to the disk
-  */
-  n[0] = cos(Crack->theta * PETSC_PI/180.)*sin(Crack->phi * PETSC_PI/180.);
-  n[1] = sin(Crack->theta * PETSC_PI/180.)*sin(Crack->phi * PETSC_PI/180.);
-  n[2] = cos(Crack->phi * PETSC_PI/180.);
-  
-  /*
-    tau: projection onto the disk plane
-  */
-  xdotn  = (x[0]-PennyCrack->corner[0])*n[0] + 
-           (x[1]-PennyCrack->corner[1])*n[1] + 
-           (x[2]-PennyCrack->corner[2])*n[2];
-  tau[0] = (x[0]-PennyCrack->corner[0]) - xdotn*n[0];
-  tau[1] = (x[1]-PennyCrack->corner[1]) - xdotn*n[1];
-  tau[2] = (x[2]-PennyCrack->corner[2]) - xdotn*n[2];
-  l = sqrt(tau[0]*tau[0] + tau[1]*tau[1] + tau[2]*tau[2]);
-  if (PennyCrack->r == 0.) {
-    *d = sqrt(pow(x[0] - PennyCrack->center[0],2) +  
-              pow(x[1] - PennyCrack->center[1],2) + 
-              pow(x[2] - PennyCrack->center[2],2));
-  } else {
-    if (l < PennyCrack->r) {
-      *d = sqrt(xdotn*xdotn);
-    } else {
-      *d = sqrt(pow(x[0] - PennyCrack->center[0] - tau[0] / l * PennyCrack->r,2) +  
-                pow(x[1] - PennyCrack->center[1] - tau[1] / l * PennyCrack->r,2) + 
-                pow(x[2] - PennyCrack->center[2] - tau[2] / l * PennyCrack->r,2));
-    } 
-  }
-  PetscFunctionReturn(0);
-}
+// #undef __FUNCT__
+// #define __FUNCT__ "VFRectangularCrackGet"
+// /*
+//   
+// 
+//   VFRectangularCrackGet (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
+// */
+// extern PetscErrorCode VFRectangularCrackGet(const char prefix[],VFRectangularCrack *RectangularCrack)
+// {
+//   PetscErrorCode ierr;
+//   PetscInt       nval=3;
+//   PetscFunctionBegin;
+//   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,prefix,"\n\nVF: Rectangular-shaped crack description:","");CHKERRQ(ierr);
+//   {
+//     ierr = PetscOptionsString("-name","\n\tRectangular-shaped crack name","",RectangularCrack->name,RectangularCrack->name,sizeof(RectangularCrack->name),PETSC_NULL);CHKERRQ(ierr);
+//     nval = 9;    
+//     ierr = PetscOptionsRealArray("-corners","\n\tRectangular-shaped crack corners coordinates (x0,y0,z0, x1,y1,z1, x2,y2,z2)  (comma separated).","",RectangularCrack->center,&nval,PETSC_NULL);CHKERRQ(ierr);
+//   }
+//   ierr = PetscOptionsEnd();CHKERRQ(ierr);
+//   PetscFunctionReturn(0);
+// }
+// 
+// #undef __FUNCT__
+// #define __FUNCT__ "VFRectangularCrackCreate"
+// /*
+//   VFRectangularCrackCreate: Allocates a RectangularCrack data structure
+//   
+//   (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
+// */
+// extern PetscErrorCode VFRectangularCrackCreate(VFRectangularCrack *RectangularCrack)
+// {
+//   PetscErrorCode ierr;
+//   int            i;
+//   
+//   PetscFunctionBegin;
+//   ierr = PetscStrcpy(RectangularCrack->name,"RectangularCrack");CHKERRQ(ierr);
+//   for (i=0; i<9; i++) RectangularCrack->corners[i] = 0.;
+//   PetscFunctionReturn(0);
+// }
+// 
+// #undef __FUNCT__
+// #define __FUNCT__ "VFRectangularCrackView"
+// /*
+//   VFRectangularCrackView 
+//   
+//   (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
+// */
+// extern PetscErrorCode VFRectangularCrackView(VFRectangularCrack *RectangularCrack,PetscViewer viewer)
+// {
+//   PetscErrorCode ierr;
+//   
+//   PetscFunctionBegin;
+//   ierr = PetscViewerASCIIPrintf(viewer,"RectangularCrack object \"%s\":\n",RectangularCrack->name);CHKERRQ(ierr);
+//   ierr = PetscViewerASCIIPrintf(viewer,"corners:     \t%e \t%e \t%e \t%e\n\t\t%e \t%e \t%e \t%e\n\t\t%e \t%e \t%e \t%e\n",
+//                                RectangularCrack->corners[0],RectangularCrack->corners[1],RectangularCrack->corners[2],
+//                                RectangularCrack->corners[3],RectangularCrack->corners[4],RectangularCrack->corners[5],
+//                                RectangularCrack->corners[6],RectangularCrack->corners[7],RectangularCrack->corners[8]);CHKERRQ(ierr);
+//   PetscFunctionReturn(0);
+// }
+// 
+// #undef __FUNCT__
+// #define __FUNCT__ "VFRectangularCrackSetName"
+// /*
+//   VFRectangularCrackSetName 
+//   
+//   (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
+// */
+// extern PetscErrorCode VFRectangularCrackSetName(VFRectangularCrack *RectangularCrack,const char name[])
+// {
+//   PetscErrorCode ierr;
+//   
+//   PetscFunctionBegin;
+//   ierr = PetscStrcpy(RectangularCrack->name,name);CHKERRQ(ierr);
+//   PetscFunctionReturn(0);
+// }
+// 
+// #undef __FUNCT__
+// #define __FUNCT__ "VFDistanceToRectangularCrack"
+// /*
+//   VFDistanceToRectangularCrack: Computes the distance between a point with coordinates x and a RectangularCrack described by RectangularCrack
+//   
+//   
+//   (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
+// */
+// extern PetscErrorCode VFDistanceToRectangularCrack(PetscReal *d,PetscReal *x,VFRectangularCrack *Crack)
+// {
+//   PetscErrorCode      ierr;
+//   PetscReal           n[3],tau[3],l,xdotn;
+// 
+//   PetscFunctionBegin;
+//   /*
+//     n: normal vector to the disk
+//   */
+//   n[0] = cos(Crack->theta * PETSC_PI/180.)*sin(Crack->phi * PETSC_PI/180.);
+//   n[1] = sin(Crack->theta * PETSC_PI/180.)*sin(Crack->phi * PETSC_PI/180.);
+//   n[2] = cos(Crack->phi * PETSC_PI/180.);
+//   
+//   /*
+//     tau: projection onto the disk plane
+//   */
+//   xdotn  = (x[0]-PennyCrack->corner[0])*n[0] + 
+//            (x[1]-PennyCrack->corner[1])*n[1] + 
+//            (x[2]-PennyCrack->corner[2])*n[2];
+//   tau[0] = (x[0]-PennyCrack->corner[0]) - xdotn*n[0];
+//   tau[1] = (x[1]-PennyCrack->corner[1]) - xdotn*n[1];
+//   tau[2] = (x[2]-PennyCrack->corner[2]) - xdotn*n[2];
+//   l = sqrt(tau[0]*tau[0] + tau[1]*tau[1] + tau[2]*tau[2]);
+//   if (PennyCrack->r == 0.) {
+//     *d = sqrt(pow(x[0] - PennyCrack->center[0],2) +  
+//               pow(x[1] - PennyCrack->center[1],2) + 
+//               pow(x[2] - PennyCrack->center[2],2));
+//   } else {
+//     if (l < PennyCrack->r) {
+//       *d = sqrt(xdotn*xdotn);
+//     } else {
+//       *d = sqrt(pow(x[0] - PennyCrack->center[0] - tau[0] / l * PennyCrack->r,2) +  
+//                 pow(x[1] - PennyCrack->center[1] - tau[1] / l * PennyCrack->r,2) + 
+//                 pow(x[2] - PennyCrack->center[2] - tau[2] / l * PennyCrack->r,2));
+//     } 
+//   }
+//   PetscFunctionReturn(0);
+// }
 
