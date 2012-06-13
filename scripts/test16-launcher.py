64d1ea0b63824cea785882880b3c011cc9d6d54b
Blaise Bourdin <bourdin@lsu.edu>
2012-06-13 18:16:03 -0500

    scripts tweaks

---
+++ scripts/test16-launcher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+
+nx = 101
+ny = 101
+nz = 101
+lx = 1.0
+ly = 1.0
+lz = 1.0
+h = lx/nx
+eta = 1e-8
+#for k in (.25,.5,1,1.5,2,2.5,3):
+#for k in (.5,1,1.5):
+#for k in (2,2.5,3):
+#for k in (2.5,3.0):
+#for k in (.25,.25):
+
+k = 1.5
+eps = k * h
+gc = 1./(1.+h/2./eps)
+runcmd = 'echo qsub -l h_rt=01:00:00 -q development -pe 12way 240 -v EPS=%f,GC=%f,NX=%i,NY=%i,ETA=%f ${VFDIR}/scripts/test16.sge'%(eps,gc,nx,ny,eta)
+os.system(runcmd)
--- scripts/test28-launcher.py
+++ scripts/test28-launcher.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 import os
 
-nx = 707
-ny = 708
+nx = 355
+ny = 355
 lx = 8.0
 h = lx/nx
 eta = 1e-8
+theta=20
 #for k in (.25,.5,1,1.5,2,2.5,3):
 #for k in (.5,1,1.5):
 #for k in (2,2.5,3):
-for k in (2.5,3.0):
+for k in (1.,1.5,2.):
 #for k in (.25,.25):
     eps = k * h
     gc = 1./(1.+h/2./eps)
-    runcmd = 'qsub -l h_rt=01:00:00 -q development -pe 12way 192 -v EPS=%f,GC=%f,NX=%i,NY=%i,ETA=%f ${VFDIR}/scripts/test15.sge'%(eps,gc,nx,ny,eta)
+    runcmd = 'qsub -l h_rt=01:00:00 -q development -pe 12way 192 -v EPS=%f,GC=%f,NX=%i,NY=%i,ETA=%f,THETA=%f ${VFDIR}/scripts/test28.sge'%(eps,gc,nx,ny,eta,theta)
     os.system(runcmd)
