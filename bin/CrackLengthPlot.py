85c447cc4ee203910128b2ab9d40dcbb8191a01e
Blaise Bourdin <bourdin@lsu.edu>
2013-01-11 09:34:35 -0600

    Removed tabs in test16.c

--- bin/CrackLengthPlot.py
+++ bin/CrackLengthPlot.py
@@ -23,6 +23,7 @@ def parse(args=None):
     #parser.add_argument('inputfile',type=argparse.FileType('r'),nargs='*',help='Input file',default=sys.stdin)
     parser.add_argument('inputfile',nargs='*',help='Input file',default=sys.stdin)
     parser.add_argument('-o','--outputfile',help='output file',default=None)
+    parser.add_argument('-d','--dim',help='Dimension',type=int,default=2)
     parser.add_argument('-t','--title',help='Title',default=None)
     return parser.parse_args()
 
@@ -40,7 +41,8 @@ def main():
         if (os.path.isfile(f)):
             p = np.loadtxt(f)   
             pp = p[:,3]
-            pp -= p[0,3]
+            if options.dim ==2:
+                pp -= p[0,3]
             plt.plot(p[:,1],pp,'--',lw=2,label=f)
         if (os.path.isdir(f)):
             infotxt = os.path.join(f,'00_INFO.txt')
@@ -52,25 +54,34 @@ def main():
                     p = np.loadtxt(presfile)    
                     hx = D['LX']/(D['NX']+0.0)
                     l = '$h=%.2E$, $\epsilon/h=%.2f\ (%s)$'%(hx,D['EPSILON']/hx,f)
+                    l = '$h=%.2E$, $\epsilon/h=%.2f$'%(hx,D['EPSILON']/hx)
                     #plt.plot(p[:,1]/D['LZ'],p[:,3]/D['LZ'],label=l,lw=2)
                     pp = p[:,3]
-                    pp -= p[0,3]
+                    if options.dim ==2:
+                        pp -= p[0,3]
                     plt.plot(p[:,1],pp,label=l,lw=2)
                 
     plt.legend(loc=0)
-    plt.xlabel('V')
-    plt.ylabel('l')
+    if options.dim ==2:
+        plt.xlabel('Injected volume $(\mathrm{m}^2)$')
+        plt.ylabel('Crack increment $(\mathrm{m})$')
+    else:
+        plt.xlabel('Injected volume $(\mathrm{m}^3)$')
+        plt.ylabel('Crack radius $(\mathrm{m})$')
 
     ax = plt.gca()
     ax.grid()
-    ax.axis([0,.025,0,.015])
+    #ax.axis([0,.025,0,.015])
     plt.legend(loc=0,labelspacing=.1)
     leg = plt.gca().get_legend()
     ltext  = leg.get_texts()
     plt.setp(ltext, fontsize='small')
 
     if not options.title:
-        plt.title('Crack increment vs. injected volume')
+        if options.dim ==2:
+            plt.title('Crack increment vs. injected volume')
+        else:
+            plt.title('Crack radius vs. injected volume')
     else:
         plt.title(options.title)
         
