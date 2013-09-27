2fd0db07181c4dc3a1ac7f881c5ff51db7b37d25
Blaise Bourdin <bourdin@lsu.edu>
2013-09-27 09:07:31 -0500

    Added a simple test of a 1D uniaxial traction to validate AT1

---
+++ ValidationTests/test1.c
@@ -0,0 +1,272 @@
+/*
+ test1.c:
+ 1D tension experiment
+ 
+ (c) 2010-2012 Blaise Bourdin bourdin@lsu.edu
+ */
+
+#include "petsc.h"
+#include "CartFE.h"
+#include "VFCommon.h"
+#include "VFMech.h"
+#include "VFFlow.h"
+
+#undef __FUNCT__
+#define __FUNCT__ "fieldsWrite"
+PetscErrorCode fieldsWrite(VFCtx *ctx,PetscViewer viewer){
+  PetscErrorCode ierr;
+  Vec            UDof;
+  PetscInt       c;
+  char           fieldNameDof[FILENAME_MAX];
+
+  ierr = DMGetGlobalVector(ctx->daScal,&UDof);CHKERRQ(ierr);
+  for (c = 0; c < 3; c++) {
+    ierr = VecStrideGather(ctx->fields->U,c,UDof,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = PetscSNPrintf(fieldNameDof,FILENAME_MAX,"%s_%i","Displacement",c);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject)UDof,fieldNameDof);CHKERRQ(ierr);
+    ierr = VecView(UDof,viewer);CHKERRQ(ierr);
+  }
+  ierr = DMRestoreGlobalVector(ctx->daScal,&UDof);CHKERRQ(ierr);
+  ierr = VecView(ctx->fields->V,viewer);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "boundaryConditionsInitialize"
+PetscErrorCode boundaryConditionsInitialize(VFCtx *ctx,PetscInt orientation){
+  PetscInt       i,j,c;
+  PetscErrorCode ierr;
+  
+  PetscFunctionBegin;
+  for (i = 0; i < 6; i++) {
+    ctx->bcV[0].face[i] = NONE;
+    for (j = 0; j < 3; j++) {
+      ctx->bcU[j].face[i] = NONE;
+    }
+  }
+  for (i = 0; i < 12; i++) {
+    ctx->bcV[0].edge[i] = NONE;
+    for (j = 0; j < 3; j++) {
+      ctx->bcU[j].edge[i] = NONE;
+    }
+  }
+  for (i = 0; i < 8; i++) {
+    ctx->bcV[0].vertex[i] = NONE;
+    for (j = 0; j < 3; j++) {
+      ctx->bcU[j].vertex[i] = NONE;
+    }
+  }
+  switch (orientation) {
+    case 0:
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Applying traction Dirichlet conditions on faces X0 X1\n");CHKERRQ(ierr);
+      for (c = 0; c < 3; c++) {
+        ctx->bcU[c].face[X0] = FIXED;
+        ctx->bcU[c].face[X1] = FIXED;
+      }
+      ctx->bcV[0].face[X0] = ONE;
+      ctx->bcV[0].face[X1] = ONE;
+      break;
+    case 1:
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Applying traction Dirichlet conditions on faces Y0 Y1\n");CHKERRQ(ierr);
+      for (c = 0; c < 3; c++) {
+        ctx->bcU[c].face[Y0] = FIXED;
+        ctx->bcU[c].face[Y1] = FIXED;
+      }
+      ctx->bcV[0].face[Z0] = ONE;
+      ctx->bcV[0].face[Z1] = ONE;
+      break;
+    case 2:
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Applying traction Dirichlet conditions on faces Z0 Z1\n");CHKERRQ(ierr);
+      for (c = 0; c < 3; c++) {
+        ctx->bcU[c].face[Z0] = FIXED;
+        ctx->bcU[c].face[Z1] = FIXED;
+      }
+      ctx->bcV[0].face[Z0] = ONE;
+      ctx->bcV[0].face[Z1] = ONE;
+      break;
+    default:
+      SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_USER,"ERROR: Orientation should be between 0 and 2, got %i\n",orientation);
+      break;
+  }
+      PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "boundaryDisplacementInitialize"
+PetscErrorCode boundaryDisplacementInitialize(VFCtx *ctx,PetscInt orientation,PetscReal bcMag,Vec BCU) {
+  PetscErrorCode ierr;
+  PetscReal      ****bcu_array;
+  PetscInt       i,j,k,nx,ny,nz,xs,xm,ys,ym,zs,zm;
+
+  PetscFunctionBegin;
+  ierr = DMDAGetInfo(ctx->daVect,PETSC_NULL,&nx,&ny,&nz,PETSC_NULL,PETSC_NULL,PETSC_NULL,
+                     PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+  ierr = DMDAGetCorners(ctx->daVect,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
+  ierr = DMDAVecGetArrayDOF(ctx->daVect,BCU,&bcu_array);CHKERRQ(ierr);
+  switch (orientation) {
+    case 0:
+      for (k = zs; k < zs+zm; k++) {
+        for (j = ys; j < ys+ym; j++) {
+          for (i = xs; i < xs+xm; i++) {
+            if (i == 0) {
+              bcu_array[k][j][i][0] = -bcMag;
+              bcu_array[k][j][i][1] = 0.;
+              bcu_array[k][j][i][2] = 0.;
+            }
+            if (i == nx-1) {
+              bcu_array[k][j][i][0] = bcMag;
+              bcu_array[k][j][i][1] = 0.;
+              bcu_array[k][j][i][2] = 0.;
+            }
+          }
+        }
+      }
+      
+      break;
+      
+    case 1:
+      for (k = zs; k < zs+zm; k++) {
+        for (j = ys; j < ys+ym; j++) {
+          for (i = xs; i < xs+xm; i++) {
+            if (j == 0) {
+              bcu_array[k][j][i][0] = 0.;
+              bcu_array[k][j][i][1] = -bcMag;
+              bcu_array[k][j][i][2] = 0.;
+            }
+            if (j == ny-1) {
+              bcu_array[k][j][i][0] = 0.;
+              bcu_array[k][j][i][1] = bcMag;
+              bcu_array[k][j][i][2] = 0.;
+            }
+          }
+        }
+      }
+      break;
+      
+    case 2:
+      for (k = zs; k < zs+zm; k++) {
+        for (j = ys; j < ys+ym; j++) {
+          for (i = xs; i < xs+xm; i++) {
+            if (k == 0) {
+              bcu_array[k][j][i][0] = 0.;
+              bcu_array[k][j][i][1] = 0.;
+              bcu_array[k][j][i][2] = -bcMag;
+            }
+            if (k == nz-1) {
+              bcu_array[k][j][i][2] = 0.;
+              bcu_array[k][j][i][2] = 0.;
+              bcu_array[k][j][i][2] = bcMag;
+            }
+          }
+        }
+      }
+      break;
+      
+    default:
+      SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_USER,"ERROR: Orientation should be between 0 and 2, got %i\n",orientation);
+      break;
+  }
+  ierr = DMDAVecRestoreArrayDOF(ctx->daVect,BCU,&bcu_array);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **argv)
+{
+  VFCtx          ctx;
+  VFFields       fields;
+  PetscErrorCode ierr;
+  PetscViewer    viewer;
+  Vec            Vold;
+  PetscReal      errV;
+  PetscInt       altminit;
+  char           filename[FILENAME_MAX];
+  
+  ierr = PetscInitialize(&argc,&argv,(char*)0,banner);CHKERRQ(ierr);
+  ierr = VFInitialize(&ctx,&fields);CHKERRQ(ierr);
+  
+  PetscInt  orientation = 2;
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-orientation",&orientation,PETSC_NULL);CHKERRQ(ierr);
+  ctx.matprop[0].beta  = 0.;
+  ctx.matprop[0].alpha = 0.;
+  
+  ierr = VecSet(fields.V,1.0);CHKERRQ(ierr);
+  ierr = VecDuplicate(fields.V,&Vold);CHKERRQ(ierr);
+  ierr = VecSet(fields.VIrrev,1.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.U,0.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.BCU,0.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.theta,0.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.thetaRef,0.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.pressure,0.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.pressureRef,0.0);CHKERRQ(ierr);
+  ierr = VecSet(fields.BCU,0.0);CHKERRQ(ierr);
+
+  ierr = boundaryConditionsInitialize(&ctx,orientation);
+  
+  for (ctx.timestep = 0; ctx.timestep < ctx.maxtimestep; ctx.timestep++){
+    if (ctx.maxtimestep > 1) {
+      ctx.timevalue = ctx.maxtimevalue / (PetscReal) (ctx.maxtimestep-1) * (PetscReal) (ctx.timestep);
+    } else {
+      ctx.timevalue = ctx.maxtimevalue;
+    }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"==== time step %i: t = %e\n",ctx.timestep,ctx.timevalue);CHKERRQ(ierr);
+    ierr = boundaryDisplacementInitialize(&ctx,orientation,ctx.timevalue,ctx.fields->BCU);
+    ierr = VFTimeStepPrepare(&ctx,&fields);CHKERRQ(ierr);
+    
+    altminit = 0;
+    errV = 1.e+10;
+    do {
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Step %i / %i\n",ctx.timestep,altminit);CHKERRQ(ierr);
+      ierr = VecCopy(fields.V,Vold);
+      ierr = VF_StepU(&fields,&ctx);
+      ierr = VF_StepV(&fields,&ctx);
+
+      ierr = VecAXPY(Vold,-1.,fields.V);CHKERRQ(ierr);
+      ierr = VecNorm(Vold,NORM_INFINITY,&errV);CHKERRQ(ierr);
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"      Max. change on V: %e\n",errV);CHKERRQ(ierr);
+      altminit++;
+    } while (errV >= ctx.altmintol && altminit <= ctx.altminmaxit);
+    ierr = VecCopy(fields.V,fields.VIrrev);CHKERRQ(ierr);
+    
+    ctx.SurfaceEnergy = 0.;
+    ctx.ElasticEnergy = 0;
+    ctx.InsituWork    = 0;
+    ctx.PressureWork  = 0.;
+    
+    ierr = VF_UEnergy3D(&ctx.ElasticEnergy,&ctx.InsituWork,&ctx.PressureWork,&fields,&ctx);CHKERRQ(ierr);
+    ierr = VF_VEnergy3D(&ctx.SurfaceEnergy,&fields,&ctx);CHKERRQ(ierr);
+    
+    ctx.TotalEnergy = ctx.ElasticEnergy-ctx.InsituWork-ctx.PressureWork;
+    
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Elastic Energy:            %e\n",ctx.ElasticEnergy);CHKERRQ(ierr);
+    if (ctx.hasCrackPressure) {
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Work of pressure forces:   %e\n",ctx.PressureWork);CHKERRQ(ierr);
+    }
+    if (ctx.hasInsitu) {
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Work of insitu stresses:   %e\n",ctx.InsituWork);CHKERRQ(ierr);
+    }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Total Mechanical energy:  %e\n",ctx.ElasticEnergy-ctx.InsituWork-ctx.PressureWork);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Surface  energy:          %e\n",ctx.SurfaceEnergy);CHKERRQ(ierr);
+    ctx.TotalEnergy = ctx.ElasticEnergy-ctx.InsituWork-ctx.PressureWork + ctx.SurfaceEnergy;
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Total  energy:            %e\n",ctx.TotalEnergy);CHKERRQ(ierr);
+		ierr = PetscViewerASCIIPrintf(ctx.energyviewer,"%d \t\t%e \t%e \t%e \t%e \t%e \t%e\n",ctx.timestep,ctx.timevalue,ctx.SurfaceEnergy,ctx.ElasticEnergy,ctx.PressureWork,ctx.InsituWork,ctx.TotalEnergy);
+    
+    /*
+     Save fields and write statistics about current run
+     */
+    ierr = PetscSNPrintf(filename,FILENAME_MAX,"%s.%.5i.vts",ctx.prefix,ctx.timestep);CHKERRQ(ierr);
+    ierr = PetscViewerCreate(PETSC_COMM_WORLD,&viewer);CHKERRQ(ierr);
+    ierr = PetscViewerSetType(viewer,PETSCVIEWERVTK);CHKERRQ(ierr);
+    ierr = PetscViewerFileSetName(viewer,filename);CHKERRQ(ierr);
+    ierr = fieldsWrite(&ctx,viewer);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+    
+    ierr = FieldsVTKWrite(&ctx,&fields,PETSC_NULL);
+  }
+  ierr = VecDestroy(&Vold);CHKERRQ(ierr);
+  ierr = VFFinalize(&ctx,&fields);CHKERRQ(ierr);
+  ierr = PetscFinalize();
+  return(0);
+}
+
