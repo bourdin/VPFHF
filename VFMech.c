1bf4c2fa03624f6d7468172cea02fc7257dd743c
Blaise Bourdin <bourdin@lsu.edu>
2014-08-01 08:47:19 -0500

    More work on TAO for U for unilateral contact

--- VFMech.c
+++ VFMech.c
@@ -952,7 +952,7 @@ extern PetscErrorCode VF_ElasticEnergy3D_local(PetscReal *ElasticEnergy_local,Pe
   PetscInt       g,i,j,k;
   PetscReal      *epsilon11_elem,*epsilon22_elem,*epsilon33_elem,*epsilon12_elem,*epsilon23_elem,*epsilon13_elem;
   PetscReal      *sigma11_elem,*sigma22_elem,*sigma33_elem,*sigma12_elem,*sigma23_elem,*sigma13_elem;
-  PetscReal      lambda,mu,alpha,coefbeta;
+  PetscReal      lambda,mu,alpha,threekappa,coefbeta;
   PetscReal      *v_elem;
   PetscErrorCode ierr;
   
@@ -965,7 +965,8 @@ extern PetscErrorCode VF_ElasticEnergy3D_local(PetscReal *ElasticEnergy_local,Pe
   lambda   = matprop->lambda;
   mu       = matprop->mu;
   alpha    = matprop->alpha;
-  coefbeta = matprop->beta / (3.*lambda + 2. *mu);
+  threekappa = (3.*lambda + 2. *mu);
+  coefbeta = matprop->beta / threekappa;
   
   for (g = 0; g < e->ng; g++) v_elem[g] = 0.;
   for (k = 0; k < e->nphiz; k++) {
@@ -1035,6 +1036,105 @@ extern PetscErrorCode VF_ElasticEnergy3D_local(PetscReal *ElasticEnergy_local,Pe
   ierr = PetscFree(v_elem);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
+
+#undef __FUNCT__
+#define __FUNCT__ "VF_ElasticEnergyNoCompression3D_local"
+/*
+ VF_ElasticEnergyNoCompression3D_local:
+ 
+ (c) 2010-2014 Blaise Bourdin bourdin@lsu.edu
+ */
+extern PetscErrorCode VF_ElasticEnergyNoCompression3D_local(PetscReal *ElasticEnergy_local,PetscReal ****u_array,PetscReal ***v_array,PetscReal ***theta_array,PetscReal ***thetaRef_array,PetscReal ***pressure_array,VFMatProp *matprop,VFProp *vfprop,PetscInt ek,PetscInt ej,PetscInt ei,VFCartFEElement3D *e)
+{
+  PetscInt       g,i,j,k;
+  PetscReal      *trepsilon_elem;
+  PetscReal      *D11_elem,*D22_elem,*D33_elem,*D12_elem,*D23_elem,*D13_elem;
+  PetscReal      lambda,mu,alpha,threekappa,coefbeta,WD;
+  PetscReal      *v_elem,*s_elem,*pressure_elem,*theta_elem;
+  PetscErrorCode ierr;
+  
+  PetscFunctionBegin;
+  ierr     = PetscMalloc3(e->ng,&D11_elem,e->ng,&D22_elem,e->ng,&D33_elem);CHKERRQ(ierr);
+  ierr     = PetscMalloc3(e->ng,&D12_elem,e->ng,&D23_elem,e->ng,&D13_elem);CHKERRQ(ierr);
+  ierr     = PetscMalloc3(e->ng,&v_elem,e->ng,&s_elem,e->ng,&trepsilon_elem);CHKERRQ(ierr);
+  ierr     = PetscMalloc2(e->ng,&pressure_elem,e->ng,&theta_elem);CHKERRQ(ierr);
+  lambda   = matprop->lambda;
+  mu       = matprop->mu;
+  alpha    = matprop->alpha;
+  threekappa = (3.*lambda + 2. *mu);
+  coefbeta = matprop->beta / threekappa;
+  
+  for (g = 0; g < e->ng; g++) {
+    D11_elem[g] = 0.;
+    D22_elem[g] = 0.;
+    D33_elem[g] = 0.;
+    D12_elem[g] = 0.;
+    D23_elem[g] = 0.;
+    D13_elem[g] = 0.;
+    trepsilon_elem[g] = 0.;
+    v_elem[g] = 0.;
+    pressure_elem[g] = 0.;
+    theta_elem[g] = 0.;
+  }
+  for (k = 0; k < e->nphiz; k++) {
+    for (j = 0; j < e->nphiy; j++) {
+      for (i = 0; i < e->nphix; i++) {
+        for (g = 0; g < e->ng; g++) {
+          D11_elem[g] +=  e->dphi[k][j][i][0][g] * u_array[ek+k][ej+j][ei+i][0];
+          D22_elem[g] +=  e->dphi[k][j][i][1][g] * u_array[ek+k][ej+j][ei+i][1];
+          D33_elem[g] +=  e->dphi[k][j][i][2][g] * u_array[ek+k][ej+j][ei+i][2];
+          
+          D12_elem[g] += (e->dphi[k][j][i][0][g] * u_array[ek+k][ej+j][ei+i][1]
+                                + e->dphi[k][j][i][1][g] * u_array[ek+k][ej+j][ei+i][0]) * .5;
+          D23_elem[g] += (e->dphi[k][j][i][1][g] * u_array[ek+k][ej+j][ei+i][2]
+                                + e->dphi[k][j][i][2][g] * u_array[ek+k][ej+j][ei+i][1]) * .5;
+          D13_elem[g] += (e->dphi[k][j][i][0][g] * u_array[ek+k][ej+j][ei+i][2]
+                                + e->dphi[k][j][i][2][g] * u_array[ek+k][ej+j][ei+i][0]) * .5;
+          v_elem[g] += e->phi[k][j][i][g] * v_array[ek+k][ej+j][ei+i];
+          pressure_elem[g] += e->phi[k][j][i][g] * pressure_array[ek+k][ej+j][ei+i];
+          theta_elem[g] += e->phi[k][j][i][g] * (theta_array[ek+k][ej+j][ei+i] - thetaRef_array[ek+k][ej+j][ei+i]);
+        }
+      }
+    }
+  }
+  *ElasticEnergy_local = 0.;
+  for (g = 0; g < e->ng; g++) {
+    trepsilon_elem[g] =  D11_elem[g] + D22_elem[g] + D33_elem[g];
+    if (trepsilon_elem[g] <= UNILATERAL_THRES) {
+      s_elem[g] = 1.;
+    } else {
+      s_elem[g] = v_elem[g];
+    }
+     
+    D11_elem[g] -= trepsilon_elem[g] / 3.;
+    D22_elem[g] -= trepsilon_elem[g] / 3.;
+    D33_elem[g] -= trepsilon_elem[g] / 3.;
+    
+    /* 
+      Deviatoric part
+      mu (1+eta) * v^2e^D:e^D
+    */
+    *ElasticEnergy_local += (( D11_elem[g] * D11_elem[g]
+                             + D22_elem[g] * D22_elem[g]
+                             + D33_elem[g] * D33_elem[g]) * .5
+                             + D12_elem[g] * D12_elem[g]
+                             + D23_elem[g] * D23_elem[g]
+                             + D13_elem[g] * D13_elem[g]) * v_elem[g] * v_elem[g] * (1+vfprop->eta) * e->weight[g];
+    /*
+      Spherical part
+      9\kappa/2 [s(tr(e)/3 - \alpha \theta)  - \beta p / 3/\kappa)]:[s(tr(e)/3 - \alpha \theta)  - \beta p / 3/\kappa)]
+    */
+    WD = (trepsilon_elem[g] / 3. - alpha * theta_elem[g]) * s_elem[g] - coefbeta * pressure_elem[g];
+    *ElasticEnergy_local += 3 * threekappa / 2. * WD * WD * e->weight[g];
+    *ElasticEnergy_local += threekappa / 6. * trepsilon_elem[g] * trepsilon_elem[g] * vfprop->eta * e->weight[g];
+  }
+  ierr = PetscFree3(D11_elem,D22_elem,D33_elem);CHKERRQ(ierr);
+  ierr = PetscFree3(D12_elem,D23_elem,D13_elem);CHKERRQ(ierr);
+  ierr = PetscFree3(v_elem,s_elem,trepsilon_elem);CHKERRQ(ierr);
+  ierr = PetscFree2(pressure_elem,theta_elem);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
 
 #undef __FUNCT__
 #define __FUNCT__ "VF_PressureWork3D_local"
@@ -1337,13 +1437,21 @@ extern PetscErrorCode VF_UEnergy3D(PetscReal *ElasticEnergy,PetscReal *InsituWor
         hy   = coords_array[ek][ej+1][ei][1]-coords_array[ek][ej][ei][1];
         hz   = coords_array[ek+1][ej][ei][2]-coords_array[ek][ej][ei][2];
         ierr = VFCartFEElement3DInit(&ctx->e3D,hx,hy,hz);CHKERRQ(ierr);
-        /*
-         Elastic Energy is trivial
-         */
-        ierr = VF_ElasticEnergy3D_local(&myElasticEnergyLocal,u_array,v_array,
-                                        theta_array,thetaRef_array,pressure_array,
-                                        &ctx->matprop[ctx->layer[ek]],&ctx->vfprop,
-                                        ek,ej,ei,&ctx->e3D);CHKERRQ(ierr);
+
+        switch (ctx->unilateral) {
+          case UNILATERAL_NONE:
+            ierr = VF_ElasticEnergy3D_local(&myElasticEnergyLocal,u_array,v_array,
+                                            theta_array,thetaRef_array,pressure_array,
+                                            &ctx->matprop[ctx->layer[ek]],&ctx->vfprop,
+                                            ek,ej,ei,&ctx->e3D);CHKERRQ(ierr);
+            break;
+          case UNILATERAL_NOCOMPRESSION:
+            ierr = VF_ElasticEnergyNoCompression3D_local(&myElasticEnergyLocal,u_array,v_array,
+                                            theta_array,thetaRef_array,pressure_array,
+                                            &ctx->matprop[ctx->layer[ek]],&ctx->vfprop,
+                                            ek,ej,ei,&ctx->e3D);CHKERRQ(ierr);
+            break;
+        }
         myElasticEnergy += myElasticEnergyLocal;
         if (ctx->hasCrackPressure) {
           ierr = VF_PressureWork3D_local(&myPressureWork,u_array,v_array,pressure_array,
