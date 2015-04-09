34a4a137dd49cdd4e9bdbdc469b9e5f28730ea8f
Chukwudi Chukwudozie <chuckdii2002@yahoo.com>
2015-04-09 11:55:40 -0500

    uncommited deletions from VFPermfield.c

--- VFPermfield.c
+++ VFPermfield.c
@@ -299,8 +299,6 @@ extern PetscErrorCode UpdateFractureWidth(VFCtx *ctx, VFFields *fields)
   PetscReal       ave_V;
   Vec             pmult_local;
 	PetscReal       ***pmult_array;
-  Vec             V;
-  Vec             U;
   Vec             coordinates;
   Vec             coords_local;
   PetscReal       coords1[3]= {0,0,0};
@@ -315,18 +313,11 @@ extern PetscErrorCode UpdateFractureWidth(VFCtx *ctx, VFFields *fields)
                      PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
   ierr = DMDAGetCorners(ctx->daWScalCell,&xs,&ys,&zs,&xm,&ym,&zm);CHKERRQ(ierr);
   ierr = DMDAGetGhostCorners(ctx->daWScalCell,&xs1,&ys1,&zs1,&xm1,&ym1,&zm1);CHKERRQ(ierr);
-//  ierr = DMCreateGlobalVector(ctx->daWScal,&V);CHKERRQ(ierr);
-//  ierr = VecSet(V,0.0);CHKERRQ(ierr);
-//  ierr = VecCopy(fields->V,V);CHKERRQ(ierr);
   ierr = DMGetLocalVector(ctx->daWScal,&v_local);CHKERRQ(ierr);
 	ierr = DMGlobalToLocalBegin(ctx->daWScal,fields->V,INSERT_VALUES,v_local);CHKERRQ(ierr);
 	ierr = DMGlobalToLocalEnd(ctx->daWScal,fields->V,INSERT_VALUES,v_local);CHKERRQ(ierr);
 	ierr = DMDAVecGetArray(ctx->daWScal,v_local,&v_array);CHKERRQ(ierr);
-  
-//  ierr = DMCreateGlobalVector(ctx->daWVect,&U);CHKERRQ(ierr);
-//  ierr = VecSet(U,0.0);CHKERRQ(ierr);
-//  ierr = VecCopy(fields->U,U);CHKERRQ(ierr);
-  
+    
   ierr = DMGetLocalVector(ctx->daWVect,&u_local);CHKERRQ(ierr);
 	ierr = DMGlobalToLocalBegin(ctx->daWVect,fields->U,INSERT_VALUES,u_local);CHKERRQ(ierr);
 	ierr = DMGlobalToLocalEnd(ctx->daWVect,fields->U,INSERT_VALUES,u_local);CHKERRQ(ierr);
@@ -506,8 +497,6 @@ extern PetscErrorCode UpdateFractureWidth(VFCtx *ctx, VFFields *fields)
 	ierr = DMLocalToGlobalBegin(ctx->daScalCell,pmult_local,INSERT_VALUES,fields->pmult);CHKERRQ(ierr);
 	ierr = DMLocalToGlobalEnd(ctx->daScalCell,pmult_local,INSERT_VALUES,fields->pmult);CHKERRQ(ierr);
   
-//  ierr = VecDestroy(&U);CHKERRQ(ierr);
-//  ierr = VecDestroy(&V);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
