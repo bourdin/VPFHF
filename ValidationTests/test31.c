afd989206e926e92886274f40f9f3f6eeb596d8c
Chukwudi Chukwudozie <chuckdii2002@yahoo.com>
2015-05-21 03:08:23 -0500

    test31.c/test31.opts modified to reflect correct input force

--- ValidationTests/test31.c
+++ ValidationTests/test31.c
@@ -1,7 +1,7 @@
 /*
  test31.c: 2D Mandel's problem. Coupling of flow and displacmement solver for geomechanics application.
  See http://scholar.lib.vt.edu/theses/available/etd-07012008-115136/unrestricted/Dissertation_ImsooLee.pdf
-  
+ 
  ./test31 -options_file test31.opts
  */
 
@@ -26,7 +26,7 @@ int main(int argc,char **argv)
 	PetscReal		lx,ly,lz;
   PetscReal   vol,vol1,vol2,vol3,vol4,vol5;
   PetscReal   pressure;
-  PetscReal   stress;
+  PetscReal   F;
   PetscReal   displ;
   PetscReal   SumnIntegral = 0;
   PetscReal   displ_p_tol = 1e-6;
@@ -34,7 +34,7 @@ int main(int argc,char **argv)
   Vec         PreIteSol;
 	PetscReal   norm_inf = 1e+3;
   PetscInt    displ_iter = 0;
-
+  
 	ierr = PetscInitialize(&argc,&argv,(char*)0,banner);CHKERRQ(ierr);
 	ierr = VFInitialize(&ctx,&fields);CHKERRQ(ierr);
   ctx.hasFlowWells = PETSC_FALSE;
@@ -46,8 +46,8 @@ int main(int argc,char **argv)
   lz = BBmax[2]-BBmin[2];
 	ly = BBmax[1]-BBmin[1];
 	lx = BBmax[0]-BBmin[0];
-  stress = -1.0;
-	ierr = PetscOptionsGetReal(PETSC_NULL,"-stress",&stress,PETSC_NULL);CHKERRQ(ierr);
+  F = -2.5;
+	ierr = PetscOptionsGetReal(PETSC_NULL,"-F",&F,PETSC_NULL);CHKERRQ(ierr);
   ierr = VFTimeStepPrepare(&ctx,&fields);CHKERRQ(ierr);
   ierr = PetscViewerCreate(PETSC_COMM_WORLD,&viewer);CHKERRQ(ierr);
   ierr = PetscViewerSetType(viewer,PETSCVIEWERASCII);CHKERRQ(ierr);
@@ -58,9 +58,10 @@ int main(int argc,char **argv)
   ierr = VecDuplicate(fields.pressure,&PreIteSol);
   ctx.timestep = 0;
   ierr = PetscPrintf(PETSC_COMM_WORLD,"  Computing initial time step solution..................\n");CHKERRQ(ierr);
-  displ = lz*(1.+ctx.matprop[0].nu)*((1.-ctx.matprop[0].nu)*stress*lx*ly+ctx.matprop[0].beta*SumnIntegral*(1.-2*ctx.matprop[0].nu))/(lx*ly*ctx.matprop[0].E);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"  Computing initial time step solution,F = %e, F*lx = %e..................\n", F,F*lx);CHKERRQ(ierr);
+  displ = lz*(1.+ctx.matprop[0].nu)*(2*(1.-ctx.matprop[0].nu)*F+ctx.matprop[0].beta*SumnIntegral*(1.-2*ctx.matprop[0].nu))/(lx*ly*ctx.matprop[0].E);
   ierr = PetscPrintf(PETSC_COMM_WORLD,"   Initial guess displacement: %g\n",displ);CHKERRQ(ierr);
-  ierr = VecSet(fields.BCU,displ);CHKERRQ(ierr);  
+  ierr = VecSet(fields.BCU,displ);CHKERRQ(ierr);
   ierr = VF_StepU(&fields,&ctx);CHKERRQ(ierr);
   ierr = VF_StepP(&fields,&ctx);
   ierr = VF_IntegrateOnBoundary(&SumnIntegral,fields.pressure,Z1,&ctx);CHKERRQ(ierr);
@@ -68,7 +69,7 @@ int main(int argc,char **argv)
   ierr = VecCopy(fields.pressure,PreIteSol);CHKERRQ(ierr);
   while (norm_inf > displ_p_tol) {
     ierr = PetscPrintf(PETSC_COMM_WORLD,"   Iteration Step: %d\n",displ_iter);CHKERRQ(ierr);
-    displ = lz*(1.+ctx.matprop[0].nu)*((1.-ctx.matprop[0].nu)*stress*lx*ly+ctx.matprop[0].beta*SumnIntegral*(1.-2*ctx.matprop[0].nu))/(lx*ly*ctx.matprop[0].E);
+    displ = lz*(1.+ctx.matprop[0].nu)*(2*(1.-ctx.matprop[0].nu)*F+ctx.matprop[0].beta*SumnIntegral*(1.-2*ctx.matprop[0].nu))/(lx*ly*ctx.matprop[0].E);
     ierr = PetscPrintf(PETSC_COMM_WORLD,"   Update displacement: %g \t Displ_error = %g\n",displ,norm_inf);CHKERRQ(ierr);
     ierr = VecSet(fields.BCU,displ);CHKERRQ(ierr);
     ierr = VF_StepU(&fields,&ctx);CHKERRQ(ierr);
@@ -114,7 +115,7 @@ int main(int argc,char **argv)
       ierr = VF_StepP(&fields,&ctx);
       ierr = VF_IntegrateOnBoundary(&SumnIntegral,fields.pressure,Z1,&ctx);CHKERRQ(ierr);
       ierr = PetscPrintf(PETSC_COMM_WORLD,"\n Integrand = %f \n",SumnIntegral);CHKERRQ(ierr);
-      displ = lz*(1.+ctx.matprop[0].nu)*((1.-ctx.matprop[0].nu)*stress*lx*ly+ctx.matprop[0].beta*SumnIntegral*(1.-2*ctx.matprop[0].nu))/(lx*ly*ctx.matprop[0].E);
+      displ = lz*(1.+ctx.matprop[0].nu)*(2*(1.-ctx.matprop[0].nu)*F+ctx.matprop[0].beta*SumnIntegral*(1.-2*ctx.matprop[0].nu))/(lx*ly*ctx.matprop[0].E);
       ierr = PetscPrintf(PETSC_COMM_WORLD,"   Initial guess displacement: %g\n",displ);CHKERRQ(ierr);
       ierr = VecSet(fields.BCU,displ);CHKERRQ(ierr);
       ierr = VF_StepU(&fields,&ctx);CHKERRQ(ierr);
@@ -151,4 +152,3 @@ int main(int argc,char **argv)
 	ierr = PetscFinalize();
 	return(0);
 }
-
