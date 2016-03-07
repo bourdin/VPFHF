d0907aa717c451f18dcae650abe380d6ce9f557a
Chukwudi Chukwudozie <chuckdii2002@yahoo.com>
2016-03-07 15:49:08 -0600

    modified test36 to verify width computation using tip removal

--- ValidationTests/test36a.c
+++ ValidationTests/test36a.c
@@ -1,9 +1,9 @@
 /*
- *  test36.c: 2D/3D. Coupled reservoir and fracture flow example.
+ *  test36a.c: 2D/3D. Coupled reservoir and fracture flow example.
  *   (c) 2012-2014 Chukwudi Chukwudozie cchukw1@tigers.lsu.edu
  *
  *    Verifies application of tip removal for a pressurized line fracture
- *     
+ *
  *
  *      */
 
@@ -23,18 +23,16 @@ VFFields            fields;
 int main(int argc,char **argv)
 {
 	PetscErrorCode  ierr;
-  PetscReal       p = 1e-6;
 
 	ierr = PetscInitialize(&argc,&argv,(char*)0,banner);CHKERRQ(ierr);
 	ierr = VFInitialize(&ctx,&fields);CHKERRQ(ierr);
 
-  
-  p = 1e-3;
   ctx.timestep = 0;
-  ierr = PetscOptionsGetReal(PETSC_NULL,"-po",&p,PETSC_NULL);CHKERRQ(ierr);
-  ierr = VecSet(fields.pressure,p);CHKERRQ(ierr);
   ierr = VFTimeStepPrepare(&ctx,&fields);CHKERRQ(ierr);
   ierr = VF_StepV(&fields,&ctx);CHKERRQ(ierr);
+  ierr = VecSet(fields.pressure,0.);CHKERRQ(ierr);
+
+  ierr = FieldsVTKWrite(&ctx,&fields,NULL,NULL);CHKERRQ(ierr);
 
 //  Normal width computation without tip effect for translated domain
   ctx.removeTipEffect = PETSC_FALSE;
@@ -44,25 +42,6 @@ int main(int argc,char **argv)
   ierr = UpdateFractureWidth(&ctx,&fields);CHKERRQ(ierr);
   ierr = FieldsVTKWrite(&ctx,&fields,NULL,NULL);CHKERRQ(ierr);
   
-//  Normal width computation with tip effect for translated domain
-  ctx.timestep = 1;
-  ctx.removeTipEffect = PETSC_TRUE;
-  ierr = PetscPrintf(PETSC_COMM_WORLD," STEP 1\n ");CHKERRQ(ierr);
-  ierr = VF_StepU(&fields,&ctx);CHKERRQ(ierr);
-  ierr = VolumetricCrackOpening(&ctx.CrackVolume, &ctx, &fields);CHKERRQ(ierr);
-  ierr = UpdateFractureWidth(&ctx,&fields);CHKERRQ(ierr);
-  ierr = FieldsVTKWrite(&ctx,&fields,NULL,NULL);CHKERRQ(ierr);
-  
-//  Normal width computation without tip effect
-  ctx.timestep = 2;
-  ctx.removeTipEffect = PETSC_FALSE;
-  ierr = VecSet(fields.BCU,0.0);CHKERRQ(ierr);
-  ierr = VecSet(fields.U,0.);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD," STEP 2  \n ");CHKERRQ(ierr);
-  ierr = VF_StepU(&fields,&ctx);CHKERRQ(ierr);
-  ierr = VolumetricCrackOpening(&ctx.CrackVolume, &ctx, &fields);CHKERRQ(ierr);
-  ierr = UpdateFractureWidth(&ctx,&fields);CHKERRQ(ierr);
-  ierr = FieldsVTKWrite(&ctx,&fields,NULL,NULL);CHKERRQ(ierr);
 
 	ierr = VFFinalize(&ctx,&fields);CHKERRQ(ierr);
 	ierr = PetscFinalize();
